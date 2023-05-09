644ae173c8ac481073c7f420263c93f3220c8b46
devholiday <devholidaydev@gmail.com>
2023-05-09 17:01:45 +0300

    update

--- src/common/components/ingredients.js
+++ src/common/components/ingredients.js
@@ -1,6 +1,7 @@
 import {useRouter} from 'next/router'
 import styles from '@/src/styles/Product.module.css'
 import {getPriceFormat} from '@/src/common/utils/currency';
+import CheckCircleSVG from '@/public/icons/check-circle';
 
 export default function Ingredients({ingredients=[], selectIngredient, watchIngr}) {
     const { locale } = useRouter();
@@ -13,6 +14,8 @@ export default function Ingredients({ingredients=[], selectIngredient, watchIngr
                 {ingredients.map(ingredient => (
                     <div key={ingredient.id} onClick={() => selectIngredient(ingredient.id)} 
                     className={styles.ingredient + ' ' + styles[isIngredientById(ingredient.id) ? 'activeIngr' : '']}>
+                        <div className={styles.checkIcon + ' ' + styles[isIngredientById(ingredient.id) ? 'selectedIcon' : '']}>
+                            <CheckCircleSVG fill='#f2790c' width='24px' height='24px' /></div>
                         {ingredient.image && <div className={styles.ingrImg}><img src={ingredient.image.src} /></div>}
                         <div className={styles.ingrTitle}>{ingredient.title[locale]}</div>
                         <div className={styles.ingrPrice}>&#8362;{getPriceFormat(ingredient.price)}</div>
